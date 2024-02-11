f76443d84f914ef83f70a0970e5332a35b78c5b0
felix2300 <Felix.Hornberger@stud.uni-regensburg.de>
2024-02-11 23:47:21 +0100

    First Implementation of Userfeedback
    The user should be able to see why he does not get to the next page still a bit bumpy (the feedback is very general, you could go into more detail about which likert-scale is the problem), but it's a start

--- components/buttons/PostTaskButton.tsx
+++ components/buttons/PostTaskButton.tsx
@@ -3,6 +3,8 @@ import { useTopicGradingStore } from '@/src/topicgrades';
 import { usePageStore } from '@/src/pagecounter';
 import { useTimeDataStore } from '@/src/time'; 
 import next from 'next';
+import { useState } from 'react';
+import PostTaskUserFeedback from '../PostTaskUserFeedback';
 
 
 export default function PostTaskButton(){
@@ -10,14 +12,20 @@ export default function PostTaskButton(){
     const topicGrads = useTopicGradingStore();
     const setTime = useTimeDataStore((state) => state.setTimeData);
     const nextPage = usePageStore((state) => state.increse);
+    const [showUserFeedback, setVisbility] = useState(false);
 
     const handleClick = () => {
         if (topicGrads['topicGrading']['evalGrade'] !== ''){
             setTime({postTask: new Date().toLocaleTimeString()});
             nextPage(1);
+        } else {
+            setVisbility(true);
         }
     }
     return(
-        <button className="bg-custom-accent p-2 text-custom-accent-text font-semibold mt-3" onClick={() => handleClick()}>Continue</button>
+        <>
+            {showUserFeedback && <PostTaskUserFeedback/>}
+            <button className="bg-custom-accent p-2 text-custom-accent-text font-semibold mt-3" onClick={() => handleClick()}>Continue</button>
+        </>
     )
 }
--- components/buttons/PreStudyButton.jsx
+++ components/buttons/PreStudyButton.jsx
@@ -5,6 +5,8 @@ import { useMildnessStore } from '@/src/mildness'
 import { selectTask } from "@/utils/selecttasktopic";
 import { useTaskTopicStore } from '@/src/tasktopic';
 import { useTimeDataStore } from '@/src/time';
+import { useState } from 'react';
+import PreStudyUserFeedback from '../PreStudyUserFeedback';
 
 function PreStudyButton() {
 
@@ -12,7 +14,10 @@ function PreStudyButton() {
   const nextPage = usePageStore((state) => state.increse);
   const setMildness = useMildnessStore((state) => state.setMildness);
   const setTaskTopic = useTaskTopicStore((state) => state.setTaskTopic);
+  const taskTopic = useTaskTopicStore();
   const setTime = useTimeDataStore((state) => state.setTimeData);
+
+  const [showUserFeedback, setVisbility] = useState(false);
 
   const handleClick = () => {
     if (topicGrads['topicGrading']['firstTopic'] !== '' &&
@@ -41,12 +46,17 @@ function PreStudyButton() {
       }
       setTime({preStudy: new Date().toLocaleTimeString()})
       nextPage(1);
+    } else {
+      setVisbility(true);
     }
   }
   return (
-    <button onClick={() => handleClick()} className="bg-custom-accent p-2 text-custom-accent-text font-semibold mt-3">
-      Continue
-    </button>
+    <>
+      {showUserFeedback && <PreStudyUserFeedback/>}
+      <button onClick={() => handleClick()} className="bg-custom-accent p-2 text-custom-accent-text font-semibold mt-3">
+        Continue
+      </button>
+    </>
   )
 }
 
