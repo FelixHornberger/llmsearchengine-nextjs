9487b33a07e7ae5fc17194919701e1cbd73be9d6
felix2300 <Felix.Hornberger@stud.uni-regensburg.de>
2024-02-22 19:04:19 +0100

    Added standard meta tag (viewport)

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,7 +2,8 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang="en"> 
+      <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0"/>  
       <Head />
       <body className={'flex flex-col bg-gray-700 items-center justify-center mt-3 text-lg text-custom-text'}>
         <Main />
