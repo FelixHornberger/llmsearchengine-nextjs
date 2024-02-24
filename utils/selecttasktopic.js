4b68bbecdd8cf846f1618ff3a8e9b2291a32426e
felix2300 <Felix.Hornberger@stud.uni-regensburg.de>
2024-02-25 00:38:09 +0100

    Update selecttasktopic.js
    Removed Console logging

--- utils/selecttasktopic.js
+++ utils/selecttasktopic.js
@@ -1,15 +1,11 @@
 export function selectTask(obj) {
     // Without removing this part of the obj it cant generate the right topic :F
     delete obj.evalGrade;
-    console.log(obj)
     let topics = Object.keys(obj).filter(key => obj[key] >= -1 && obj[key] <= 1);
-    console.log("1: ", topics)
     // If there are no mild topics, check for -2 or 2
     if (topics.length === 0) {
         topics = Object.keys(obj).filter(key => obj[key] === -2 || obj[key] === 2);
-        console.log("2: ", topics)
         if (topics.length === 0) {
-            console.log("3: ", topics)
             // If there are no mild, -2, or 2 topics, check for -3 or 3
             topics = Object.keys(obj).filter(key => obj[key] === -3 || obj[key] === 3);
         }
