f656888635101f7bb753b8a42f0935b5d8d6dcb1
felix2300 <Felix.Hornberger@stud.uni-regensburg.de>
2024-02-24 13:16:37 +0100

    Fixed Intro Page
    Made the parent div of our Welcome Text to flex and justify-center so that it centers on big screens

--- components/pages/Intro.jsx
+++ components/pages/Intro.jsx
@@ -6,13 +6,13 @@ function Intro({nextPage, addTime}){
         <div className="text-center place-items-center mb-3">
             <h1 >Welcome to our Study</h1>
         </div>
-        <div className="px-5">
-            <div className="task-box mb-3 max-w-prose place-items-center px-5 border border-white ">
+        <div className="px-5 flex justify-center">
+            <div className="task-box mb-3 max-w-prose place-items-center px-5 border border-white">
                 <p className="text-padding text-center">
                 The purpose of this research is to investigate how Generative Information Retrieval systems and their
-                source materials influence users' attitudes and interaction patterns regarding contentious topics. The
+                source materials influence users&apos attitudes and interaction patterns regarding contentious topics. The
                 goal of this study is to analyze how the Generative Information Retrieval systems and answers
-                provided by GIR System (and the way these are generated) affect users' perspectives and engagement
+                provided by GIR System (and the way these are generated) affect users&apos perspectives and engagement
                 behaviors on controversial subjects. Your participation will help us achieve this research goal. The
                 results of this research may be presented at scientific or professional meetings or published in
                 scientific proceedings and journals.
