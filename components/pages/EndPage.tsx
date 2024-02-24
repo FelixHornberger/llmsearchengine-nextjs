9eb4d8dd53ab847d2e27946c477e69d7f0d4510c
felix2300 <Felix.Hornberger@stud.uni-regensburg.de>
2024-02-24 13:17:47 +0100

    Basic Info for upcomig Sites (Task-desc & End)
    Read the comments of the pages

---
+++ components/pages/EndPage.tsx
@@ -0,0 +1,6 @@
+/*
+    This file will contain the end-screen
+    TODO:
+    Writing a text in which we thank the users for being part of the study. Here we show the users their SurveyID, as we have written in the information consent that users can contact us if their data should be deleted.
+    In addition, we have to define a new state called StudentStore (boolean). If this is true, we show users the possibility to get VP hours.
+*/
---
+++ components/pages/TaskDescription.tsx
@@ -0,0 +1,5 @@
+/*
+    This ist just a Page where we define the Text:
+    This is the page where the user is only shown the description of the next page.
+    We need to introduce a new variable in the TimeStore state where the submit is tracked. Whereby we could theoretically understand this as task time? But I don't know how clever that is?
+*/
