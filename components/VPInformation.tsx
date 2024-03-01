f277c89ce0223863261a854816c62fbea71a0261
felix2300 <Felix.Hornberger@stud.uni-regensburg.de>
2024-03-01 03:27:05 +0100

    Restyle of VP-Information
    Its now in the style of the PostSurvey-Page

--- components/VPInformation.tsx
+++ components/VPInformation.tsx
@@ -6,27 +6,30 @@ import Martikel from "./vp-information/Matrikel";
 
 export default function VPInformation() {
     return (
-        <div className="text-center px-5">
-            <p className="mb-3">
-                Thank you for your work so far. For the last step all you have to do is
-                answer some question pretaining to your personal demographics.
-            </p>
-            <div className="mb-3">
-                <p>Firstname:</p>
-                <FirstName />
+        <div className="flex felx-col justify-center">
+            <div className="text-center sm:w-[50%] justify-center px-5">
+                <p className="mb-3">
+                    Thank you for your work so far. For the last step all you have to do is
+                    answer some question pretaining to your personal demographics.
+                </p>
+                <div className="mb-3">
+                    <p>Firstname:</p>
+                    <FirstName />
+                </div>
+                <div className="mb-3">
+                    <p>Lastname:</p>
+                    <LastName />
+                </div>
+                <div className="mb-3">
+                    <p>Degree Programm:</p>
+                    <DegreeProgramm />
+                </div>
+                <div className="mb-3">
+                    <p>Martikel:</p>
+                    <Martikel />
+                </div>
+                <SubmitVPButton />
             </div>
-            <div className="mb-3">
-                <p>Lastname:</p>
-                <LastName />
-            </div>
-            <div className="mb-3">
-                <p>Degree Programm:</p>
-                <DegreeProgramm />
-            </div>
-            <div className="mb-3">
-                <p>Martikel:</p>
-                <Martikel />
-            </div>
-            <SubmitVPButton />
-        </div>);
+        </div>
+    );
 }
