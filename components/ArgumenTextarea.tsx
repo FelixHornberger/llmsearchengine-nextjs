43282a0daa8b8c86c437e59649ced1875db31d72
felix2300 <Felix.Hornberger@stud.uni-regensburg.de>
2024-03-02 22:25:28 +0100

    ArgumentTextarea is now large-screen friendly
    Changed the max width to 75% looks much better on large screens

--- components/ArgumenTextarea.tsx
+++ components/ArgumenTextarea.tsx
@@ -10,7 +10,7 @@ interface ArgumenTextareaProps {
 export default function ArgumentTextarea({ changeArgument, argument }: ArgumenTextareaProps) {
 
   return (
-    <textarea className="w-full bg-transparent border-custom-primary border-2 border-primary focus:outline-none shadow-xl focus:ring-1 rounded-lg h-32 mt-3"
+    <textarea className="w-full sm:w-[75%] bg-transparent border-custom-primary border-2 border-primary focus:outline-none shadow-xl focus:ring-1 rounded-lg h-32 mt-3"
       value={argument['arguments']}
       placeholder="Type a message..."
       id="message-input"
