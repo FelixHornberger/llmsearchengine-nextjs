ade2a3b88f1f50b5e7ebc771b5599f37eb533ab1
NoahMeissner <108337767+NoahMeissner@users.noreply.github.com>
2024-03-03 00:31:10 +0100

    update Text intro

--- components/pages/Intro.jsx
+++ components/pages/Intro.jsx
@@ -9,7 +9,7 @@ export default function Intro() {
             <div className="px-5 flex justify-center">
                 <div className="task-box mb-3 max-w-prose place-items-center px-5 border border-white">
                     <p className="text-center">
-                        You are invited to take part in an online study investigating behaviour with a generative (chat-based) information system. The study will be conducted by Noah Meißner and Felix Hornberger and supervised by Dr David Elsweiler from the University of Regensburg. The study with an expected 69 participants will take place from 12.02.2024 to 01.03.2024. The duration of the study is 15 minutes. Students at the University of Regensburg will receive 0.25 credit hours. The study can only be started once and should be completed in one go.
+                        You are invited to take part in an online study investigating behaviour with a generative (chat-based) information system. The study will be conducted by Noah Meißner and Felix Hornberger and supervised by Dr David Elsweiler from the University of Regensburg.  The duration of the study is 15 minutes. Students at the University of Regensburg will receive 0.25 credit hours. The study can only be started once and should be completed in one go.
                     </p>
                 </div>
             </div>
