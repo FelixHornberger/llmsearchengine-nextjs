5d409e462596ea97859bb5a8325baab4f13ea4d9
felix2300 <Felix.Hornberger@stud.uni-regensburg.de>
2024-03-04 00:41:15 +0100

    Submit security check in VP-hours
    Userfeedback and security check added

--- components/buttons/SubmitVPButton.tsx
+++ components/buttons/SubmitVPButton.tsx
@@ -4,6 +4,8 @@ import { useFirstNameStore } from "@/src/firstname";
 import { useLastNameStore } from "@/src/lastname";
 import { useMatrikelStore } from "@/src/matrikel";
 import { useVPStore } from "@/src/vp";
+import UserFeedback from "../UserFeedback";
+import { useState } from "react";
 
 // TODO: Need a validation logic for this
 
@@ -17,27 +19,39 @@ export default function SubmitVPButton() {
     const { lastName } = useLastNameStore();
     const { degreeProgramm } = useDegreeProgrammStore();
 
-    const handleclick = () => {
-        setVP(false);
+    const [showUserFeedback, setVisbility] = useState(false);
 
-        const submitData = async () => {
-            const response = await fetch('/api/submitStudent', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({
-                    "matrikel": matrikel,
-                    "firstname": firstName,
-                    "lastname": lastName,
-                    "degreeProgramm": degreeProgramm,
-                    "time": new Date().toDateString()
-                }),
-            });
+    const handleclick = () => {
+        if (matrikel !== '' &&
+            firstName !== '' &&
+            lastName !== '' &&
+            degreeProgramm !== '') {
+            setVP(false);
+
+            const submitData = async () => {
+                const response = await fetch('/api/submitStudent', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify({
+                        "matrikel": matrikel,
+                        "firstname": firstName,
+                        "lastname": lastName,
+                        "degreeProgramm": degreeProgramm,
+                        "time": new Date().toDateString()
+                    }),
+                });
+            }
+            submitData();
+        } else {
+            setVisbility(true);
         }
-        submitData();
     }
     return (
-        <button className='bg-custom-accent p-2 font-semibold mt-3' onClick={() => handleclick()}>SubmitVP</button>
+        <>
+            {showUserFeedback && <UserFeedback feedbackText='In order for the VP hours to be credited, we need all the data!' />}
+            <button className='bg-custom-accent p-2 font-semibold my-3' onClick={() => handleclick()}>SubmitVP</button>
+        </>
     );
 };
